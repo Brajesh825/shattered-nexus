9b0b413f0c596d729ea7f76f1c2e97b0ada94db5
unknown <brajeshmishra825@gmail.com>
2026-04-28 11:33:41 +0530

    docs: update roadmap priorities and audit level range comments

--- js/map/map-data.js
+++ js/map/map-data.js
@@ -9,14 +9,23 @@
  *  15 cracked-stone 16 ember-pit 17 obsidian-wall
  *
  * Map files loaded from data/:
- *   map-verdant-vale.js      Arc 1   Lv 1-3
- *   map-crystal-cavern.js    Arc 1-2 Lv 5-8
- *   map-ember-wastes.js      Arc 2   Lv 8-12
- *   map-sunken-temple.js     Arc 3   Lv 18-22
- *   map-shadow-reach.js      Arc 4   Lv 14-18
- *   map-void-citadel.js      Arc 5   Lv 18-22
- *   map-fortress-ramparts.js Arc 7   Lv 22-26
- *   map-eternal-void.js      Arc 8   Lv 26-30
+ *   map-verdant-vale.js      Arc 1   Lv 1-8
+ *   map-crystal-cavern.js    Arc 2   Lv 7-15
+ *   map-ember-wastes.js      Arc 3   Lv 12-20
+ *   map-sunken-temple.js     Arc 4   Lv 17-25
+ *   map-shadow-reach.js      Arc 5   Lv 22-30
+ *   map-void-citadel.js      Arc 6   Lv 27-35
+ *   map-fortress-ramparts.js Arc 7   Lv 32-40
+ *   map-eternal-void.js      Arc 8   Lv 37-45
+ *
+ * Expansion Regions:
+ *   map-southern-isles.js      Exp 1   Lv 10-18
+ *   map-riverlands-crossing.js Exp 2   Lv 10-18
+ *   map-ashen-foothills.js     Exp 3   Lv 20-28
+ *   map-lighthouse-isles.js    Exp 4   Lv 25-33
+ *   map-eastern-wetlands.js    Exp 5   Lv 30-38
+ *   map-northern-highlands.js  Exp 6   Lv 35-43
+ *   map-sky-ruins.js           Exp 7   Lv 35-43
  *
  * Each map file registers itself via MAP_DEFS.<id> = { ... }
  */
